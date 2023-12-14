aada4d59dcbf339656ee207ef0f3ed52c35544ab
Darren Li <darrenldldev@gmail.com>
2023-12-15 00:55:12 +1100

    Updated start-container.sh to accept  as optional port argument

--- start-container.sh
+++ start-container.sh
@@ -4,10 +4,16 @@ TAMARIN_VER="1.8.0"
 
 WORK_DIR_NAME=$(basename $PWD)
 
+if [[ "$1" == "" ]]; then
+  port="3001"
+else
+  port="$1"
+fi
+
 podman run -it \
   -v "$PWD":/home/user/"$WORK_DIR_NAME" \
   --userns keep-id:uid=1000,gid=1000 \
   --workdir /home/user/"$WORK_DIR_NAME" \
   --rm \
-  -p "127.0.0.1:3001:3001" \
+  -p "127.0.0.1:$port:3001" \
   docker.io/darrenldl/tamarin-prover:"$TAMARIN_VER"
